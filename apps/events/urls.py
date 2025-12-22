492dcf0e934ffca2312cf9317e141a851b16c76c
Pavlo Starobub <starodub.pavlo@alsodev.md>
2025-12-22 16:00:14 +0200

    Refactor serializers.py

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -1,8 +1,10 @@
 from django.urls import include, path
 from rest_framework import routers
+from apps.events.views import EventViewSet
 
 app_name = "events"
 
 router = routers.DefaultRouter()
+router.register("", EventViewSet, basename="events")
 
 urlpatterns = [path("", include(router.urls))]
