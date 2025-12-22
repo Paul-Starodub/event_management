638d9d15d193955056971ccdc41ca4e94509e504
Pavlo Starobub <starodub.pavlo@alsodev.md>
2025-12-22 13:07:27 +0200

    Set up Docker with PostgreSQL and Redis integration; update settings for environment configuration

--- event_management/settings.py
+++ event_management/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = "event_management.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "NAME": os.getenv("POSTGRES_NAME"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
