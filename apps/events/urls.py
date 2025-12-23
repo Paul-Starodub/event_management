cd70ae773500075314c52c59f6f027334d128b78
Pavlo Starobub <starodub.pavlo@alsodev.md>
2025-12-23 09:36:28 +0200

    Refactor urls for users

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -1,4 +1,3 @@
-from django.urls import include, path
 from rest_framework import routers
 from apps.events.views import EventViewSet
 
@@ -7,4 +6,4 @@ app_name = "events"
 router = routers.DefaultRouter()
 router.register("", EventViewSet, basename="events")
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = router.urls
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-from apps.users.views import CreateUserView, ManageUserView, LogoutAPIView
+from apps.users.views import CreateUserView, ManageUserView, LogoutAPIView, UserViewSet
 
 app_name = "users"
+
+router = routers.DefaultRouter()
+
+router.register(r"all", UserViewSet, basename="all-users")
 
 urlpatterns = [
     path("", CreateUserView.as_view(), name="create"),
@@ -11,4 +16,5 @@ urlpatterns = [
     path("logout/", LogoutAPIView.as_view(), name="logout"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("", include(router.urls)),
 ]
